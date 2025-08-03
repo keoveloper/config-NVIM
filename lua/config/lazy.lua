6b201113327e34618bef56813d42381e5a579e72
keobandolero <octav0773@gmail.com>
2025-08-03 16:24:27 -0600

    Add copilot chat instead of avante
    Well in this commit is all funcion but i really don't know if i can use
    claude

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,6 +25,16 @@ require("lazy").setup({
 			},
 		},
 		{
+			"ravitemer/mcphub.nvim",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+			},
+			build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+			config = function()
+				require("mcphub").setup()
+			end,
+		},
+		{
 			"rest-nvim/rest.nvim",
 			dependencies = {
 				"nvim-treesitter/nvim-treesitter",
